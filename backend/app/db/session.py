bf588f10925900d759011e3693de52807804b7d2
404brtk <bzielonka1@proton.me>
2026-02-14 15:45:12 +0100

    style: migrate from declarative_base() to DeclarativeBase class

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from app.config import get_settings
 
 settings = get_settings()
@@ -12,7 +12,9 @@ engine = create_engine(
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+
+class Base(DeclarativeBase):
+    pass
 
 
 def get_db():
